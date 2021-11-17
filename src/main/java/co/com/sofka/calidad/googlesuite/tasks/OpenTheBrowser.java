2004cb4aa84148f1d83e911dd5bedb6b6eabd6f4
Juan Camargo <juan.camargo@moove-it.com>
2021-11-17 12:18:39 -0500

    build.gradle fix

--- src/main/java/co/com/sofka/calidad/googlesuite/tasks/OpenTheBrowser.java
+++ src/main/java/co/com/sofka/calidad/googlesuite/tasks/OpenTheBrowser.java
@@ -1,4 +1,11 @@
 package co.com.sofka.calidad.googlesuite.tasks;
 
-public class OpenTheBrowser implements Task{
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+
+public class OpenTheBrowser implements Task {
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+
+    }
 }
